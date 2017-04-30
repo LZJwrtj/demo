298167ec13cb05e1c8660fc8652651e8fd65fbc3
LZJwrtj <zhij_web@163.com>
2017-04-30 23:36:14 +0800

    Update goods.vue

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -85,10 +85,11 @@
     },
     created () {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
-      this.$http.get('/api/goods').then((response) => {
+      this.$http.get('static/data.json').then((response) => {
         response = response.body;
         if (response.errno === ERR_OK) {
-          this.goods = response.data;
+          //this.goods = response.data;
+          this.goods = res.data.goods;
           this.$nextTick(() => {
             this._initScroll();
             this._calculateHeight();
